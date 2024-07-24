9c1a247ef2200ca087621f447d63e59a4323a957
Deivid de Jesus <imdeividsilva@gmail.com>
2024-07-24 15:02:54 -0300

    atualização nos valores

--- src/foods.ts
+++ src/foods.ts
@@ -3,41 +3,33 @@ export const carbos = [
     {
         name: 'Arroz Tipo 1 Cozido',
         carb: 2.8,
-        prot: 0.2,
+        prot: 0.3,
         fat: 0,
-        kcal: 12
+        kcal: 12.8
     },
 
     {
         name: 'Batata Inglesa Cozida',
-        carb: 1.1,
+        carb: 1.2,
         prot: 0.1,
         fat: 0,
-        kcal: 5
+        kcal: 5.2
     },
 
     {
         name: 'Batata Doce Cozida',
         carb: 1.8,
-        prot: 0,
+        prot: 0.1,
         fat: 0,
-        kcal: 7
+        kcal: 7.7
     },
 
     {
         name: 'Mandioca Cozida',
         carb: 3,
-        prot: 0,
+        prot: 0.1,
         fat: 0,
-        kcal: 12
-    },
-
-    {
-        name: 'Pão de forma integral',
-        carb: 4.9,
-        prot: 0.9,
-        fat: 0.3,
-        kcal: 25
+        kcal: 12.5
     }
-
+    
 ]
