2f31e42c5fe19b06ef8c0cc1d64a9f00892233a5
Deivid de Jesus <imdeividsilva@gmail.com>
2024-07-24 12:30:18 -0300

    criação do foods.ts

---
+++ src/foods.ts
@@ -0,0 +1,43 @@
+export const carbos = [
+    
+    {
+        name: 'Arroz Tipo 1 Cozido',
+        carb: 2.8,
+        prot: 0.2,
+        fat: 0,
+        kcal: 12
+    },
+
+    {
+        name: 'Batata Inglesa Cozida',
+        carb: 1.1,
+        prot: 0.1,
+        fat: 0,
+        kcal: 5
+    },
+
+    {
+        name: 'Batata Doce Cozida',
+        carb: 1.8,
+        prot: 0,
+        fat: 0,
+        kcal: 7
+    },
+
+    {
+        name: 'Mandioca Cozida',
+        carb: 3,
+        prot: 0,
+        fat: 0,
+        kcal: 12
+    },
+
+    {
+        name: 'Pão de forma integral',
+        carb: 4.9,
+        prot: 0.9,
+        fat: 0.3,
+        kcal: 25
+    }
+
+]
