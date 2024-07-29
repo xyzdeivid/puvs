4c3a7e86df3a984bd26490e037c82f7abde8265d
Deivid de Jesus <imdeividsilva@gmail.com>
2024-07-29 14:49:22 -0300

    FoodForm.tsx - creation of function createFood

--- src/components/DietPage/FoodForm.tsx
+++ src/components/DietPage/FoodForm.tsx
@@ -2,38 +2,60 @@ import { useState } from 'react'
 
 import FormStyle from 'components/common/FormStyle'
 import FoodNameInput from './FoodFormComponents/FoodNameInput'
-import CarboInput from './FoodFormComponents/CarboInput'
-import ProtInput from './FoodFormComponents/ProtInput'
-import FatInput from './FoodFormComponents/FatInput'
-import CaloriesInput from './FoodFormComponents/CaloriesInput'
+import NumberInput from './FoodFormComponents/NumberInput'
 import ConfirmButton from 'components/common/Form/ConfirmButton'
 
 import { formcontainerStyle } from 'styles'
-import NumberInput from './FoodFormComponents/NumberInput'
 
 export default function FoodForm() {
 
-    const [foodName, setFoodName] = useState('')
+    const [name, setName] = useState('')
     const [amount, setAmount] = useState(0)
-    const [carbo, setCarbo] = useState('')
-    const [prot, setProt] = useState('')
-    const [fat, setFat] = useState('')
-    const [calories, setCalories] = useState('')
+    const [carbo, setCarbo] = useState(0)
+    const [prot, setProt] = useState(0)
+    const [fat, setFat] = useState(0)
+    const [calories, setCalories] = useState(0)
+
+    function createFood() {
+        const food = {
+            name,
+            amount,
+            carbo,
+            prot,
+            fat,
+            calories
+        }
+    }
 
     return (
         <div style={formcontainerStyle}>
             <FormStyle>
-                <form>
-                    <FoodNameInput setFoodName={setFoodName} />
+                <form onSubmit={e => {
+                    e.preventDefault()
+                    createFood()
+                }}>
+                    <FoodNameInput setFoodName={setName} />
                     <NumberInput
                         labelText='Quantidade (g)'
                         setValue={setAmount}
                     />
                     <hr />
-                    <CarboInput />
-                    <ProtInput />
-                    <FatInput />
-                    <CaloriesInput />
+                    <NumberInput
+                        labelText='Carboidrato (g)'
+                        setValue={setCarbo}
+                    />
+                    <NumberInput
+                        labelText='Proteína (g)'
+                        setValue={setProt}
+                    />
+                    <NumberInput
+                        labelText='Gordura (g)'
+                        setValue={setFat}
+                    />
+                    <NumberInput
+                        labelText='Calorias (kcal)'
+                        setValue={setCalories}
+                    />
                     <ConfirmButton text='Registrar' />
                 </form>
             </FormStyle>
