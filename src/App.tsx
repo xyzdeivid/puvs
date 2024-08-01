c05dd5f45036d9a8507c93a238ba6c33f5632bdb
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-01 15:20:46 -0300

    Diet.tsx - Food.tsx file removal

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from 'pages/Home'
 import User from 'pages/User'
-import DietPage from 'pages/DietPage'
+import Diet from 'pages/Diet'
 import UserProvider from 'UserInfo'
 
 import './App.css'
@@ -15,7 +15,7 @@ export default function App() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/user' element={<User />} />
-                    <Route path='/dietpage' element={<DietPage />} />
+                    <Route path='/dietpage' element={<Diet />} />
                 </Routes>
             </Router>
         </UserProvider>
--- src/pages/Diet.tsx
+++ src/pages/Diet.tsx
@@ -5,10 +5,9 @@ import { foods } from 'types'
 import { containerStyle } from 'styles'
 
 import FoodForm from 'components/DietPage/FoodForm'
-import Foods from 'components/DietPage/Foods'
 import OpenFormButton from 'components/common/OpenFormButton'
 
-export default function DietPage() {
+export default function Diet() {
 
     const [showFoodForm, setShowFoodForm] = useState(false)
     const [foods, setFoods] = useState<foods>([])
@@ -24,11 +23,6 @@ export default function DietPage() {
             {
                 showFoodForm
                     ? <FoodForm setFoods={setFoods} setShowFoodForm={setShowFoodForm} />
-                    : null
-            }
-            {
-                foods[0]
-                    ? <Foods foods={foods} />
                     : null
             }
         </div>
