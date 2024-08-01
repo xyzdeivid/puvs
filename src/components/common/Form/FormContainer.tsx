09a66e1835687f7eda6902edcd2b91051d50ba67
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-01 12:17:35 -0300

    deleting of form.ts

--- src/components/common/Form/FormContainer.tsx
+++ src/components/common/Form/FormContainer.tsx
@@ -1,14 +1,21 @@
 import { useRef } from 'react'
 
-import { closeForm } from 'functions/form'
-
 interface FormContainerProps {
     setShowForm: React.Dispatch<React.SetStateAction<boolean>>
     children: React.ReactNode
 }
 
 export default function FormContainer({ setShowForm, children }: FormContainerProps) {
+
     const formRef = useRef(null)
+
+    function closeForm(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        if (formRef.current) {
+            if (e.target === formRef.current) {
+                setShowForm(false)
+            }
+        }
+    }
 
     const formcontainerStyle: React.CSSProperties = {
         position: 'absolute',
@@ -19,8 +26,9 @@ export default function FormContainer({ setShowForm, children }: FormContainerPr
         width: '100vw',
         height: '100vh'
     }
+
     return (
-        <div ref={formRef} onClick={e => closeForm(e, formRef, setShowForm)} style={formcontainerStyle}>
+        <div ref={formRef} onClick={e => closeForm(e)} style={formcontainerStyle}>
             {children}
         </div>
     )
--- src/functions/form.ts
+++
@@ -1,7 +0,0 @@
-export function closeForm(e: React.MouseEvent<HTMLDivElement, MouseEvent>, formRef: React.MutableRefObject<null>, setShowForm: React.Dispatch<React.SetStateAction<boolean>>) {
-    if (formRef.current) {
-        if (e.target === formRef.current) {
-            setShowForm(false)
-        }
-    }
-}
