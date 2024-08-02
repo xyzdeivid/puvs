22664add24ead3eda4e14031c1a207aae863dbaf
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-02 12:32:50 -0300

    creation of Information.tsx

---
+++ src/components/pages/User/Information.tsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+
+import { UserContext } from 'UserInfo'
+
+import { functions } from 'functions/user'
+
+import InfoBox from 'components/common/InfoBox'
+
+export default function Information() {
+
+    const user = useContext(UserContext)
+    const [weight] = user.weight
+    const [heightWrong] = user.height
+    const height = heightWrong / 100
+    const [age] = user.age
+    const [exerciseLevel] = user.exerciseLevel
+    const [sex] = user.sex
+
+    const functionsSource = functions(weight, height, age, exerciseLevel, sex)
+    const imc = functionsSource.imcCalculator()
+    const dailyCalorieExpenditure = functionsSource.dailyCalorieExpenditure()
+    const water = functionsSource.waterCalculator()
+
+    return (
+        <div>
+            <InfoBox index='IMC' text={imc} />
+            <InfoBox index='Estimativa de gasto calórico' text={`${dailyCalorieExpenditure}kcal`} />
+            <InfoBox index='Sugestão de consumo de água' text={`${water}ml`} />
+        </div>
+    )
+
+} 
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,28 +1,10 @@
-import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { UserContext } from 'UserInfo'
-
-import { functions } from 'functions/user'
 
 import { buttonStyle, mainColor, containerStyle } from 'styles'
 
-import InfoBox from 'components/common/InfoBox'
+import Information from 'components/pages/User/Information'
 
 export default function Info() {
-
-    const user = useContext(UserContext)
-    const [weight] = user.weight
-    const [heightWrong] = user.height
-    const height = heightWrong / 100
-    const [age] = user.age
-    const [exerciseLevel] = user.exerciseLevel
-    const [sex] = user.sex
-
-    // Funções utilizadas na página
-    const functionsSource = functions(weight, height, age, exerciseLevel, sex)
-    const imc = functionsSource.imcCalculator()
-    const dailyCalorieExpenditure = functionsSource.dailyCalorieExpenditure()
-    const water = functionsSource.waterCalculator()
 
     const circleStyle: React.CSSProperties = {
         width: '16px',
@@ -40,11 +22,7 @@ export default function Info() {
         <div style={containerStyle}>
             <div style={{ position: 'relative', marginBottom: '16px' }}>
                 <div style={circleStyle}></div>
-                <div>
-                    <InfoBox index='IMC' text={imc} />
-                    <InfoBox index='Estimativa de gasto calórico' text={`${dailyCalorieExpenditure}kcal`} />
-                    <InfoBox index='Sugestão de consumo de água' text={`${water}ml`} />
-                </div>
+                <Information />
             </div>
             <div>
                 <span>Crie sua dieta </span>
