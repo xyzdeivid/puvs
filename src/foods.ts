6d9ab9caa16ba290a90ae0aa3d398c12d98ab080
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-05 14:31:39 -0300

    creation of foods.ts

---
+++ src/foods.ts
@@ -0,0 +1,63 @@
+interface food {
+    name: string
+    carbohydrate: number
+    protein: number
+    fat: number
+    calories: number
+}
+
+const carbohydrates: food[] = [
+
+    {
+        name: 'Arroz Branco (Cozido)',
+        carbohydrate: 0.28,
+        protein: 0.02,
+        fat: 0.002,
+        calories: 1.3
+    },
+
+    {
+        name: 'Feijão Preto (Cozido)',
+        carbohydrate: 0.16,
+        protein: 0.06,
+        fat: 0.003,
+        calories: 0.9
+    },
+
+    {
+        name: 'Batata Inglesa (Cozida)',
+        carbohydrate: 0.18,
+        protein: 0.02,
+        fat: 0.001,
+        calories: 0.77
+    },
+
+    {
+        name: 'Batata Doce (Cozida)',
+        carbohydrate: 0.20,
+        protein: 0.01,
+        fat: 0.002,
+        calories: 0.86
+    },
+
+    {
+        name: 'Mandioca (Cozida)',
+        carbohydrate: 0.36,
+        protein: 0.01,
+        fat: 0.003,
+        calories: 1.5
+    },
+
+    {
+        name: 'Banana Prata',
+        carbohydrate: 0.23,
+        protein: 0.011,
+        fat: 0.001,
+        calories: 0.89
+    }
+
+]
+
+export const foods = {
+    carbohydratesSources: carbohydrates
+}
