53711464217378608ae2f62f0239393653b30483
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-08 11:20:49 -0300

    FoodOptionsForm.tsx - creation of defaultOption

--- src/components/pages/Diet/FoodOptionsForm.tsx
+++ src/components/pages/Diet/FoodOptionsForm.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+import { carbohydrates } from 'foods'
+
+import { food } from 'types'
+
 import FormContainer from 'components/common/Form/FormContainer'
 import FormStyle from 'components/common/Form/FormStyle'
 import StringSelectInput from 'components/common/Form/inputs/StringSelectInput'
@@ -15,6 +19,18 @@ export default function FoodOptionsForm({ setFoodOptionsForm }: FoodOptionsFormP
 
     const [source, setSource] = useState('carbohydrate')
     const [amount, setAmount] = useState(100)
+    const [food, setFood] = useState<food>(setDefaultFood())
+
+    function setDefaultFood() {
+        const defaultOption: food = {
+            name: carbohydrates[0].name,
+            carbohydrate: carbohydrates[0].carbohydrate * amount,
+            protein: carbohydrates[0].protein * amount,
+            fat: carbohydrates[0].fat * amount,
+            calories: carbohydrates[0].calories * amount
+        }
+        return defaultOption
+    }
 
     return (
         <FormContainer setShowForm={setFoodOptionsForm}>
@@ -32,7 +48,7 @@ export default function FoodOptionsForm({ setFoodOptionsForm }: FoodOptionsFormP
                         value={amount}
                     />
                     <hr />
-                    <FoodInfo />
+                    <FoodInfo food={food} />
                 </form>
             </FormStyle>
         </FormContainer>
--- src/components/pages/Diet/FoodOptionsForm/FoodInfo.tsx
+++ src/components/pages/Diet/FoodOptionsForm/FoodInfo.tsx
@@ -1,13 +1,25 @@
+import { food } from 'types'
+
 import { mainColor } from 'styles'
 
-export default function FoodInfo() {
+interface FoodInfoProps {
+    food: food
+}
+
+export default function FoodInfo({ food }: FoodInfoProps) {
+
+    function hasDecimal(number: number) {
+        return number % 1 !== 0
+            ? number.toFixed(1)
+            : number
+    }
 
     return (
         <ul style={{ marginTop: '16px' }}>
-            <li style={{ color: mainColor }}>Carboidratos:</li>
-            <li style={{ color: mainColor }}>Proteínas:</li>
-            <li style={{ color: mainColor }}>Gorduras:</li>
-            <li style={{ color: mainColor }}>Calorias</li>
+            <li><span style={{ color: mainColor, fontWeight: 'bolder' }}>Carboidratos:</span> {hasDecimal(food.carbohydrate)}g</li>
+            <li><span style={{ color: mainColor, fontWeight: 'bolder' }}>Proteínas:</span> {hasDecimal(food.protein)}g</li>
+            <li><span style={{ color: mainColor, fontWeight: 'bolder' }}>Gorduras:</span> {hasDecimal(food.fat)}g</li>
+            <li><span style={{ color: mainColor, fontWeight: 'bolder' }}>Calorias:</span> {hasDecimal(food.calories)}g</li>
         </ul>
     )
 
