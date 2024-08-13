45b1c69670d96a12dbcace50961af9485480b5b8
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-13 12:59:33 -0300

    Diet.tsx - removing of formals

--- src/pages/Diet.tsx
+++ src/pages/Diet.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'react'
-
-import { foods } from 'types'
-
 import Container from 'components/common/Container'
-import OpenFormButton from 'components/common/OpenFormButton'
-import Foods from 'components/pages/Diet/Foods'
-import FoodOptionsForm from 'components/pages/Diet/FoodOptionsForm'
 
 export default function Diet() {
 
-    const [showFoodOptionsForm, setFoodOptionsForm] = useState(false)
-    const [foods, setFoods] = useState<foods>([])
 
     return (
         <Container>
-            <OpenFormButton
-                setShowForm={setFoodOptionsForm}
-                buttonText='Adicionar Alimento'
-            />
-            {
-                showFoodOptionsForm
-                    ? <FoodOptionsForm setFoodOptionsForm={setFoodOptionsForm} />
-                    : null
-            }
-            {
-                foods[0]
-                    ? <Foods foods={foods} />
-                    : null
-            }
+            <h1>***Diet Page***</h1>
         </Container>
     )
 }
