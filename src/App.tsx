298d00af26717f86a37cd19f53660e02f3d70e99
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-19 09:26:46 -0300

    deleting of Diet.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from 'pages/Home'
 import User from 'pages/User'
-import Diet from 'pages/Diet'
 import UserProvider from 'UserInfo'
 
 import './App.css'
@@ -15,7 +14,6 @@ export default function App() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/user' element={<User />} />
-                    <Route path='/dietpage' element={<Diet />} />
                 </Routes>
             </Router>
         </UserProvider>
