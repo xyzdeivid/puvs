8cd9ff19287ab3b041cc43e9a48117bd7f40fb06
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-23 11:32:30 -0300

    Diet.tsx - deployment of OpenFormButton.tsx

--- src/pages/Diet.tsx
+++ src/pages/Diet.tsx
@@ -1,10 +1,14 @@
-import Container from "components/common/Container";
+import Container from 'components/common/Container'
+import OpenFormButton from 'components/common/OpenFormButton'
+import { useState } from 'react'
 
 export default function Diet() {
 
+    const [showAddFoodForm, setShowAddFoodForm] = useState(false)
+
     return (
         <Container>
-            <h1>Diet</h1>
+            <OpenFormButton buttonText='Adicionar Alimento' setShowForm={setShowAddFoodForm} />
         </Container>
     )
 
