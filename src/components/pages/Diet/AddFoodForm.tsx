8e21de5dd6a900d38217ee8ba1009742cc87d146
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-25 22:29:01 -0300

    creation of FoodOptionsInput

--- src/components/pages/Diet/AddFoodForm.tsx
+++ src/components/pages/Diet/AddFoodForm.tsx
@@ -5,10 +5,7 @@ import { Food } from 'types'
 import FormContainer from 'components/common/Form/FormContainer'
 import FormStyle from 'components/common/Form/FormStyle'
 import SourceInput from './AddFoodForm/SourceInput'
-import InputContainer from 'components/common/Form/inputs/InputContainer'
-import Label from 'components/common/Label'
-import CarboOptionsInput from './AddFoodForm/CarboOptionsInput'
-import ProtOptionsInput from './AddFoodForm/ProtOptionsInput'
+import FoodOptionsInput from './AddFoodForm/FoodOptionsInput'
 import FoodInformation from './AddFoodForm/FoodInformation'
 
 interface AddFoodFormProps {
@@ -20,15 +17,6 @@ export default function AddFoodForm({ setShowAddFoodForm }: AddFoodFormProps) {
     const [source, setSource] = useState('carbo')
     const [food, setFood] = useState({} as Food)
 
-    function sourceHandler() {
-        if (source === 'carbo') {
-            return <CarboOptionsInput setDefaultFood={setDefaultFood} setFood={setFood} />
-        }
-        if (source === 'prot') {
-            return <ProtOptionsInput setDefaultFood={setDefaultFood} setFood={setFood} />
-        }
-    }
-
     const setDefaultFood = useCallback((food: Food) => {
         setFood(food)
     }, [])
@@ -38,10 +26,11 @@ export default function AddFoodForm({ setShowAddFoodForm }: AddFoodFormProps) {
             <FormStyle>
                 <form>
                     <SourceInput setSource={setSource} />
-                    <InputContainer>
-                        <Label name='Alimento' />
-                        {sourceHandler()}
-                    </InputContainer>
+                    <FoodOptionsInput
+                        source={source}
+                        setDefaultFood={setDefaultFood}
+                        setFood={setFood}
+                    />
                     <FoodInformation food={food} />
                 </form>
             </FormStyle>
---
+++ src/components/pages/Diet/AddFoodForm/FoodOptionsInput.tsx
@@ -0,0 +1,32 @@
+import { Food } from 'types'
+
+import InputContainer from 'components/common/Form/inputs/InputContainer'
+import Label from 'components/common/Label'
+import CarboOptionsInput from './FoodOptionsInput/CarboOptionsInput'
+import ProtOptionsInput from './FoodOptionsInput/ProtOptionsInput'
+
+interface FoodOptionsInputProps {
+    source: string
+    setDefaultFood: (food: Food) => void
+    setFood: React.Dispatch<React.SetStateAction<Food>>
+}
+
+export default function FoodOptionsInput({ source, setDefaultFood, setFood }: FoodOptionsInputProps) {
+
+    function sourceHandler() {
+        if (source === 'carbo') {
+            return <CarboOptionsInput setDefaultFood={setDefaultFood} setFood={setFood} />
+        }
+        if (source === 'prot') {
+            return <ProtOptionsInput setDefaultFood={setDefaultFood} setFood={setFood} />
+        }
+    }
+
+    return (
+        <InputContainer>
+            <Label name='Alimento' />
+            {sourceHandler()}
+        </InputContainer>
+    )
+    
+}
