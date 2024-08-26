b07e475ef9b5177424020551703b266031c64742
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-26 12:22:28 -0300

    FoodsList.tsx - updating style

--- src/components/pages/Diet/FoodsList.tsx
+++ src/components/pages/Diet/FoodsList.tsx
@@ -6,14 +6,29 @@ interface FoodsListProps {
 
 export default function FoodsList({ dietFoods }: FoodsListProps) {
 
+    const listStyle: React.CSSProperties = {
+        display: 'flex',
+        justifyContent: 'space-between'
+    }
+
+    const infoStyle: React.CSSProperties = {
+        fontWeight: 'bolder'
+    }
+
     return (
-        <ul>
-            {dietFoods.map(food => {
-                return (
-                    <li key={food.name}>{food.name}</li>
-                )
-            })}
-        </ul>
+        <div>
+            <ul>
+                {dietFoods.map(food => {
+                    return (
+                        <li style={listStyle} key={food.name}>
+                            <span style={{ marginRight: '8px' }}>{food.name}</span>
+                            <span style={infoStyle}>Info</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <hr style={{ margin: '16px 0' }} />
+        </div>
     )
 
 }
